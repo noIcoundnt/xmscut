35fa1e9af85c20dd16f4fe3f44a5158ea58007e0
lsp <1785999869@qq.com>
2023-08-17 05:40:21 +0800

    add try catch block

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,9 @@
 document.addEventListener('keydown', (event) => {
-    if (event.key === '\\') {
+  if (event.key === '\\') {
+    try {
       chrome.runtime.sendMessage({ command: "fastForward" });
+    } catch (error) {
+      console.error("发送消息时出现错误:", error);
     }
+  }
 });
