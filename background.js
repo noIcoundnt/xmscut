3bce3f3bb1a39a0f7230ad477ec062143cef052b
lsp <1785999869@qq.com>
2023-10-30 16:20:48 +0800

    add speed edition

--- background.js
+++ background.js
@@ -1,22 +1,152 @@
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.command === "fastForward") {
     console.log("[FastForwardExtension] Fast forwarding videos by 90 seconds.");
-
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var tab = tabs[0];
-      var tabId = tab.id;
-
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        func: function () {
-          var videos = document.querySelectorAll("video");
-          videos.forEach((video) => {
-            if (video) {
-              video.currentTime += 90;
-            }
-          });
-        },
-      });
+      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
+        console.log(tabs.length);
+        var tab = tabs[0];
+        console.log(tab);
+        var tabId = tab.id;
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          func: function () {
+            var videos = document.querySelectorAll("video");
+            // console.log(tabId);
+            videos.forEach((video) => {
+              if (video) {
+                video.currentTime += 90;
+              }
+            });
+          },
+        });
+      } else {
+        console.error("[FastForwardExtension] 没有当前选项卡");
+      }
     });
   }
+  else if (message.command === "fastBackward") {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
+        console.log(tabs.length);
+        
+        var tab = tabs[0];
+        console.log(tab);
+        
+        var tabId = tab.id;
+
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          func: function () {
+            var videos = document.querySelectorAll("video");
+            // console.log(tabId);
+            videos.forEach((video) => {
+              if (video) {
+                video.currentTime -= 90;
+              }
+            });
+          },
+        });
+      } else {
+        console.error("[FastForwardExtension] 没有当前选项卡");
+      }
+    });
+  }
+  else if (message.command === "fastSpeed") {
+  
+    console.log("[FastForwardExtension] Fast backwarding videos by 90 seconds.");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
+        console.log(tabs.length);
+        
+        var tab = tabs[0];
+        console.log(tab);
+        
+        var tabId = tab.id;
+
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          func: function () {
+            var videos = document.querySelectorAll("video");
+            // console.log(tabId);
+            videos.forEach((video) => {
+              if (video) {
+                video.playbackRate += 0.25;              }
+            });
+          },
+        });
+      } else {
+        console.error("[FastForwardExtension] 没有当前选项卡");
+      }
+    });
+  }
+  else if (message.command === "slowSpeed") {
+    console.log("slow speed ");
+  
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
+        console.log(tabs.length);
+        
+        var tab = tabs[0];
+        console.log(tab);
+        
+        var tabId = tab.id;
+
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          func: function () {
+            var videos = document.querySelectorAll("video");
+            // console.log(tabId);
+            videos.forEach((video) => {
+              if (video) {
+                video.playbackRate -= 0.25;
+              }
+            });
+          },
+        });
+      } else {
+        console.error("[FastForwardExtension] 没有当前选项卡");
+      }
+    });
+  }
+  else if (message.command === "showSth") {
+    console.log("slow speed ");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
+        console.log(tabs.length);
+        
+        var tab = tabs[0];
+        console.log(tab);
+        
+        var tabId = tab.id;
+
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          func: function () {
+            var videos = document.querySelectorAll("video");
+            // console.log(tabId);
+            videos.forEach((video) => {
+              if (video) {
+                video.playbackRate -= 0.25;
+              }
+            });
+          },
+        });
+      } else {
+        console.error("[FastForwardExtension] 没有当前选项卡");
+      }
+    });
+  }
+  
 });
+
+function isFocusedOnInput() {
+  //焦点检测
+  const focusedElement = document.activeElement;
+  return (
+    focusedElement && (focusedElement.tagName === 'INPUT' || focusedElement.tagName === 'TEXTAREA')
+  );
+}
--- contentScript.js
+++ contentScript.js
@@ -3,7 +3,30 @@ document.addEventListener('keydown', (event) => {
     try {
       chrome.runtime.sendMessage({ command: "fastForward" });
     } catch (error) {
-      console.error("发送消息时出现错误:", error);
+
     }
+  }else if (event.key === '\|') {
+    try {
+      chrome.runtime.sendMessage({ command: "fastBackward" });
+    } catch (error) {
+
+    }
+  }else if (event.ctrlKey) {
+    if (event.key === 'ArrowUp') {
+      // 增加视频倍速
+      try {
+        chrome.runtime.sendMessage({ command: "fastSpeed" });
+      } catch (error) {
+  
+      } 
+    }else  if (event.key === 'ArrowDown') {
+      // 增加视频倍速
+      try {
+        chrome.runtime.sendMessage({ command: "slowSpeed" });
+      } catch (error) {
+  
+      } 
+    } 
   }
+
 });
