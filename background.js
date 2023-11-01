48a063aced1341d3232a64ce17d33334f6fac753
lsp <1785999869@qq.com>
2023-11-02 00:26:18 +0800

    add e shortcut to three times fast play

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 
+let originalPlaybackRate = 1;
+//
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.command === "fastForward") {
     console.log("[FastForwardExtension] Fast forwarding videos by 90 seconds.");
@@ -112,35 +116,59 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       }
     });
   }
-  else if (message.command === "showSth") {
-    console.log("slow speed ");
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tabs.length > 0) { // 确保至少有一个 tab 选项卡
-        console.log(tabs.length);
-        
-        var tab = tabs[0];
-        console.log(tab);
-        
-        var tabId = tab.id;
+  else if (message.command === "switchLeftTab") {
+    switchTabLeft();
+  }
+  else if (message.command === "switchRightTab") {
+    switchTabRight();
+  }
+  else if (message.command === "sannpeiicecream") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var url = tabs[0].url;
 
-        chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          func: function () {
-            var videos = document.querySelectorAll("video");
-            // console.log(tabId);
-            videos.forEach((video) => {
-              if (video) {
-                video.playbackRate -= 0.25;
-              }
-            });
-          },
-        });
-      } else {
-        console.error("[FastForwardExtension] 没有当前选项卡");
+      if (url && url.includes("www.youtube.com")) {
+        if (message.keyState === "keydown") {
+          var tab = tabs[0];
+          console.log(tab);
+        
+          var tabId = tab.id;
+          
+          chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: function () {
+              var videos = document.querySelectorAll("video");
+              // console.log(tabId);
+              videos.forEach((video) => {
+                if (video) {
+                  if(video.playbackRate != 3){
+                    originalPlaybackRate = video.playbackRate;
+                    video.playbackRate = 3;    
+                  }        
+                }
+              });
+            },
+          });
+        } else if (message.keyState === "keyup") {
+          var tab = tabs[0];
+          console.log(tab);
+        
+          var tabId = tab.id;
+          chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: function () {
+              var videos = document.querySelectorAll("video");
+              // console.log(tabId);
+              videos.forEach((video) => {
+                if (video) {
+                  video.playbackRate = originalPlaybackRate ; 
+                 }
+              });
+            },
+          });
+        }
       }
     });
   }
-  
 });
 
 function isFocusedOnInput() {
@@ -149,4 +177,50 @@ function isFocusedOnInput() {
   return (
     focusedElement && (focusedElement.tagName === 'INPUT' || focusedElement.tagName === 'TEXTAREA')
   );
+}
+
+function switchTabLeft() {
+  // chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+  //   if (tabs.length > 1) {
+  //     // const currentIndex = tabs[0].index;
+  //     // const newIndex = currentIndex - 1 < 0 ? tabs.length - 1 : currentIndex - 1;
+  //     await chrome.tabs.update(tabs[tabs.length-1].id, { active: true });
+  //     await chrome.windows.update(tabs[tabs.length-1].windowId, { focused: true });
+
+  //   }
+  // });
+  chrome.tabs.query({ currentWindow: true }, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(activeTabs) {
+      var activeTab = activeTabs[0];
+      var currentIndex = tabs.findIndex(tab => tab.id === activeTab.id);
+      var newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        newIndex = tabs.length - 1;
+      }
+      chrome.tabs.update(tabs[newIndex].id, { active: true });
+    });
+  });
+}
+
+function switchTabRight() {
+  // chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+  //   if (tabs.length > 1) {
+  //     // const currentIndex = tabs[0].index;
+  //     // const newIndex = (currentIndex + 1) % tabs.length;
+  //     await chrome.tabs.update(tabs[1].id, { active: true });
+  //     await chrome.windows.update(tabs[1].windowId, { focused: true });
+
+  //   }
+  // });
+  chrome.tabs.query({ currentWindow: true }, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(activeTabs) {
+      var activeTab = activeTabs[0];
+      var currentIndex = tabs.findIndex(tab => tab.id === activeTab.id);
+      var newIndex = currentIndex + 1;
+      if (newIndex >= tabs.length) {
+        newIndex = 0;
+      }
+      chrome.tabs.update(tabs[newIndex].id, { active: true });
+    });
+  });
 }
--- contentScript.js
+++ contentScript.js
@@ -26,7 +26,55 @@ document.addEventListener('keydown', (event) => {
       } catch (error) {
   
       } 
+      
+    }else  if (event.key === 'ArrowLeft') { 
+      try {
+        event.preventDefault(); // 禁止默认行为
+
+        chrome.runtime.sendMessage({command: "switchLeftTab"})
+
+         } catch (error) {
+  
+      } 
+      
+    }else  if (event.key === 'ArrowRight') { 
+      try {
+        chrome.runtime.sendMessage({command: "switchRightTab"})
+
+         } catch (error) {
+  
+      } 
+      
     } 
   }
+  else if (event.key === 'e') {
+    event.stopPropagation();
+    try {
+      chrome.runtime.sendMessage({ command: "sannpeiicecream", keyState: "keydown" });
+    } catch (error) {
+      // 处理错误
+    }
+  }
 
+});
+
+document.addEventListener('keyup', (event) => {
+  if(event.key === 'e') {
+    event.preventDefault();
+      try {
+
+        chrome.runtime.sendMessage({ command: "sannpeiicecream", keyState: "keyup" });
+      } catch (error) {
+        // 处理错误
+      }
+    }
+  }
+);
+
+window.addEventListener('beforeunload', function (event) {
+  // 在页面关闭前执行你的清理操作
+  // 例如，保存数据或执行其他清理任务
+  console.log('Page is about to unload. Perform cleanup here.');
+  // 如果需要，可以使用 event.preventDefault() 阻止页面的关闭
+  event.preventDefault();
 });
